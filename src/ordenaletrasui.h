267bf5e3951779448f94a4a37456e81af00e563c
dalcoa00 <dalcoa00@estudiantes.unileon.es>
2023-04-12 18:37:05 +0200

    Proyecto ordenado en carpetas

---
+++ src/ordenaletrasui.h
@@ -0,0 +1,45 @@
+#ifndef ORDENALETRASUI_H
+#define ORDENALETRASUI_H
+
+#include <QDialog>
+
+#include <iostream>
+#include <ostream>
+#include <fstream>
+#include <QFileDialog>
+
+namespace Ui {
+class ordenaletrasui;
+}
+
+class ordenaletrasui : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit ordenaletrasui(QWidget *parent = nullptr);
+    ~ordenaletrasui();
+    bool static compareChars(char a, char b);
+    bool static isUnwantedChar(char c);
+    void execAlgorithm();
+
+private slots:
+    void on_execAlgoritmo_clicked();
+    void on_reset_clicked();
+
+    void on_selecArchivo_clicked();
+
+private:
+    Ui::ordenaletrasui *ui;
+
+    std::ifstream inputFile;
+    std::ofstream outputFile;
+    QString fileName;
+    std::string fileNameStr;
+    std::string outputFileNameStr;
+    std::string inputText;
+
+
+};
+
+#endif // ORDENALETRASUI_H
